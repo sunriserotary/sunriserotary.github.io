9250a78aa34c86dacdf64c31bd6745b70679dac6
Josh <JTCheung3@gmail.com>
2018-03-02 02:58:34 -0800

    updated templates and anchor links for events

--- js/index.js
+++ js/index.js
@@ -16,3 +16,14 @@ $(document).on('click', 'a[href^="#"]', function (event) {
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
 });
+
+//Smooth scrolling with links
+$('a[href*=\\#]').on('click', function(event){
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+});
+
+// Smooth scrolling when the document is loaded and ready
+$(document).ready(function(){
+  $('html,body').animate({scrollTop:$(location.hash).offset().‌​top}, 500);
+});
