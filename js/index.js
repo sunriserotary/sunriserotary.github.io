1a48556047cb00eaea327e742e26fa0960f9c044
Josh <JTCheung3@gmail.com>
2018-03-02 12:00:54 -0800

    quick fix for navbar

--- js/index.js
+++ js/index.js
@@ -22,8 +22,3 @@ $('a[href*=\\#]').on('click', function(event){
     event.preventDefault();
     $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
 });
-
-// Smooth scrolling when the document is loaded and ready
-$(document).ready(function(){
-  $('html,body').animate({scrollTop:$(location.hash).offset().‌​top}, 500);
-});
