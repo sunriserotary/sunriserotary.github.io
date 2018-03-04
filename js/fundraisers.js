4868b8c69d8d756c9988b2cb6a3c136748962224
Josh <JTCheung3@gmail.com>
2018-03-03 21:26:05 -0800

    update

--- js/fundraisers.js
+++ js/fundraisers.js
@@ -51,7 +51,7 @@ var orgchart = new Chart(org_id, {
    options: {
       responsive: true,
       legend: {
-         dispaly: false,
+         display: true,
          position: 'right'
       }
    }
